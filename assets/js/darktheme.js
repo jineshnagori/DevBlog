2d9a3ae7637593b4639a367c0a0c897c61ad2a2b
jineshnagori <jineshn21@gmail.com>
2023-10-03 21:14:07 +0530

    formatting of complete code

--- assets/js/darktheme.js
+++ assets/js/darktheme.js
@@ -3,7 +3,7 @@ var themeSheet = document.querySelector("#theme-sheet");
 
 function theme() {
     console.log("background changed");
-    if(themeSheet.getAttribute("href") == "assets/css/style.css") {
+    if (themeSheet.getAttribute("href") == "assets/css/style.css") {
         themeSheet.href = "assets/css/darktheme.css";
     } else {
         themeSheet.href = "assets/css/style.css";
