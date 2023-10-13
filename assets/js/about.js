c0b171fe671a54bdb27f582415adb5f0ed71442e
tharanithar-r <rtharanithar@gmail.com>
2023-10-14 00:34:41 +0530

    Fixed styling issues in about.html

--- assets/js/about.js
+++ assets/js/about.js
@@ -5,7 +5,7 @@ for (i = 0; i < faq.length; i++) {
   faq[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    this.classList.toggle("activefaq");
 
     /* Toggle between hiding and showing the active panel */
     var body = this.nextElementSibling;
